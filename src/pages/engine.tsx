b61319821de142d9c6c0575ee460d82968cdde67
Aaron Velasco <tiquismiquisss@gmail.com>
2019-07-21 23:24:40 +0200

    single quotes for souble quotes

--- src/pages/engine.tsx
+++ src/pages/engine.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Engine: React.FC = () => {
-  return (
-    <div>
-        Engine
-    </div>
-  );
-}
+    return <div>Engine</div>;
+};
 
 export default Engine;
