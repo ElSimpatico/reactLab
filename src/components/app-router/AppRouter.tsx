97cb1415019a5b03ce3dafdc131db8d6a112e0a3
Aaron Velasco <tiquismiquisss@gmail.com>
2019-08-15 13:44:53 +0200

    refactor(Styles): Use material-ui to create styles in js

--- src/components/app-router/AppRouter.tsx
+++ src/components/app-router/AppRouter.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Routes
 import { routes } from '../../routes';
+
+// Components
 import NavigationPages from '../navigation-pages/navigation-pages';
 
+// Styles
+import { useStyles } from './styles';
+
 const AppRouter: React.FC = () => {
+    const classes = useStyles();
+
     return (
         <Router>
-            <div
-                className={'principalClass'}
-                style={{
-                    position: 'relative',
-                    height: 'calc(100vh - 22px)',
-                    width: '100vw'
-                }}
-            >
+            <div className={classes.root}>
                 <Switch>
                     {routes.map((route, index) => {
                         return (
                             <Route
                                 key={index}
                                 path={route.path}
-                                exact={route.path === '/' ? true : false}
+                                exact={route.path === '/'}
                                 render={props => (
-                                    <div
-                                        className={'contentPages'}
-                                        style={{
-                                            margin: '20px',
-                                            overflowY: 'scroll',
-                                            overflowX: 'hidden',
-                                            maxHeight: '80vh'
-                                        }}
-                                    >
+                                    <div className={classes.wrappedContent}>
                                         <route.component {...props} />
                                     </div>
                                 )}
---
+++ src/components/app-router/styles.ts
@@ -0,0 +1,17 @@
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(() =>
+    createStyles({
+        root: {
+            position: 'relative',
+            height: 'calc(100vh - 22px)',
+            width: '100vw'
+        },
+        wrappedContent: {
+            margin: '20px',
+            overflowY: 'scroll',
+            overflowX: 'hidden',
+            maxHeight: '80vh'
+        }
+    })
+);
