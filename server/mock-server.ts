0404aad26b3b183a26de4c764cb62da6ed493af9
Aaron Velasco <tiquismiquisss@gmail.com>
2019-09-02 20:57:25 +0200

    feat(server): mock server refactor

--- server/mock-server.ts
+++ server/mock-server.ts
@@ -1,13 +1,32 @@
+/**Third-party imports */
 import JsonServer from 'json-server';
+
+/**Local imports */
 import { resolve } from 'path';
-import { config } from 'dotenv';
+import {
+    getDotEnvironment,
+    ENVIRONMENT
+} from '../environments/utils/environment';
+import { routesRewrite } from './routes-rewriter';
 
 const dbDir = resolve(__dirname, '../mocks/data.json');
-const environmentVariables = config({
-    path: resolve(__dirname, '../environments/.env.dev')
-}).parsed as any;
+const environmentVariables = getDotEnvironment(ENVIRONMENT.DEV) as any;
 
 const server = JsonServer.create();
+const serverRewriter: any = Object.keys(routesRewrite).reduce(
+    (prev: any, current: string) => {
+        const key = `${environmentVariables.API_BASE}${current}`;
+        const value = `${environmentVariables.API_BASE}${routesRewrite[current]}`;
+        prev[key] = value;
+        return prev;
+    },
+    {}
+);
+
+server.use(JsonServer.rewriter(serverRewriter));
+server.use(function(req, res, next) {
+    setTimeout(next, 500);
+});
 const router = JsonServer.router(dbDir);
 const middlewares = JsonServer.defaults();
 
---
+++ server/routes-rewriter.ts
@@ -0,0 +1,3 @@
+export const routesRewrite: any = {
+    '/cars/models': '/cars'
+};
