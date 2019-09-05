4829c354fdc7627118f6c86cd7772ced6e088a7c
Aaron Velasco <tiquismiquisss@gmail.com>
2019-09-05 20:55:24 +0200

    feat(Pages): car-selection
    Dispach setModelIdAction when click car image

--- src/pages/car-selection/car-selection.tsx
+++ src/pages/car-selection/car-selection.tsx
@@ -3,16 +3,20 @@ import React, { useState, useEffect } from 'react';
 
 /** Third-party imports */
 import Grid from '@material-ui/core/Grid';
+import { useDispatch } from 'react-redux';
 
 /** Local imports */
 import { Spinner, SIZE_TYPES } from '@shared/components';
 import { Car } from '@shared/models';
 import { CarCard } from '@components';
 import { getCars } from '@services';
+import { setModelId } from '@shared/actions';
 
 export const CarSelection: React.FC = () => {
     const [cars, setCars] = useState<Car[]>([]);
     const [isLoading, setIsLoading] = useState(true);
+
+    const dispatch = useDispatch();
 
     async function loadData() {
         let carsResponse: Car[] = [];
@@ -30,6 +34,10 @@ export const CarSelection: React.FC = () => {
         loadData();
     }, []);
 
+    function selectCarModel(car: Car) {
+        dispatch(setModelId(car.key));
+    }
+
     if (isLoading) {
         return <Spinner size={SIZE_TYPES.big} />;
     }
@@ -38,7 +46,7 @@ export const CarSelection: React.FC = () => {
             {cars.map((car: Car, index: number) => {
                 return (
                     <Grid xs={4} item key={`${car.key}_${index}`}>
-                        <CarCard car={car} />
+                        <CarCard car={car} onClick={selectCarModel} />
                     </Grid>
                 );
             })}
