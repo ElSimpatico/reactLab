bf34a34489e213e0e07cdc2388065893505b1649
Aaron Velasco <tiquismiquisss@gmail.com>
2019-09-15 13:58:33 +0200

    refactor(Components): NavigationPages
    Use withRouter

--- src/components/navigation-pages/navigation-page-props.ts
+++ src/components/navigation-pages/navigation-page-props.ts
@@ -1,5 +1,9 @@
+/** Third-party imports */
+import { RouteComponentProps } from 'react-router';
+
+/** Local imports */
 import { Router } from '@shared/routes';
 
-export interface NavigationPageProps {
+export interface NavigationPageProps extends RouteComponentProps {
     routes: Router[];
 }
