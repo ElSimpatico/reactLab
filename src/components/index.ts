a0c16a917e7eb8e5549a9d0d759b6ad29c82c1e2
Aaron Velasco <tiquismiquisss@gmail.com>
2019-10-12 10:16:39 +0200

    feat(Components): Add layout and body-card components in barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export * from './app-router/AppRouter';
+export * from './layout/layout';
 export * from './car-card/carCard';
+export * from './body-card/body-card';
 export * from './navigation-pages/navigation-pages';
 export * from './header/header';
